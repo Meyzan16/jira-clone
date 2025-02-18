8988b4c73e35354312cdca00b47fcd28113b08a1
G1A018086 <meyzan1605@gmail.com>
2025-02-18 14:23:19 +0700

    add components loader

---
+++ src/components/pulseloader.tsx
@@ -0,0 +1,44 @@
+import React, { FC } from "react";
+
+interface Props {
+    text?: string;
+    color?: string;
+    loading: boolean;
+    size?: number;
+}
+
+const PulseLoader: FC<Props> = ({ text, color, loading, size = 10 }) => {
+    if (!loading) return null;
+  
+    return (
+      <div className="flex items-center gap-2">
+        {text && <span style={{ color }}>{text}</span>} 
+        <div className="flex space-x-1" data-testid="loader">
+          <span
+            style={{ backgroundColor: color, width: size, height: size }}
+            className="animate-bounce rounded-full"
+          />
+          <span
+            style={{
+              backgroundColor: color,
+              width: size,
+              height: size,
+              animationDelay: '150ms',
+            }}
+            className="animate-bounce rounded-full"
+          />
+          <span
+            style={{
+              backgroundColor: color,
+              width: size,
+              height: size,
+              animationDelay: '300ms',
+            }}
+            className="animate-bounce rounded-full"
+          />
+        </div>
+      </div>
+    );
+  };
+  
+  export default PulseLoader;
