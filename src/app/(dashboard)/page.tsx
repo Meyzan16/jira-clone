74799830b6e75f6da2d5d602b30ce091419c18b0
G1A018086 <meyzan1605@gmail.com>
2025-04-10 22:51:49 +0700

    changes actions auth

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,9 +1,8 @@
 import { redirect } from "next/navigation";
-import { getCurrent } from "@/features/auth/actions";
+import { getCurrent } from "@/features/auth/queries";
 import { getWorkspaces } from "@/features/workspaces/actions";
 const Home = async () => {
   const user = await  getCurrent();
-
   if(!user) redirect("/sign-in");
 
   const workspace = await getWorkspaces();
